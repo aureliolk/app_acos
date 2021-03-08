cd98278af726a43dd8a36407669360ea7a5f8430
Aurélio Castro <aurelio.cos@outlook.com>
2021-03-08 13:41:50 -0300

    Fix solution erro Link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
 import Logo from './partials/Logo';
 import Button from '../elements/Button';
 
