bd599103c7f60765618dbea67ee3bdcd4ecd4e22
Aurélio Castro <aurelio.cos@outlook.com>
2022-07-15 18:21:05 -0300

    Add Public Folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ApolloProvider } from '@apollo/client'
+import type { AppProps } from 'next/app'
+
 import { client } from '../lib/apollo'
 
 import { SkillProvider } from '../components/contexts/SkillContexts'
 import { SectionProvider } from '../components/contexts/SectionContexts'
 import { ParallaxProvider } from 'react-scroll-parallax';
 import '../styles/globals.css'
-import { AppProps } from 'next/app';
+
 
 function MyApp({ Component, pageProps }: any) {
 
